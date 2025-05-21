48725c5ef7da0ee834ec33027c164b8f01928ef0
KhanWarden <baryrhan2004@gmail.com>
2025-05-21 21:00:52 +0500

    Header refactoring

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,38 +1,56 @@
-import React, {ReactNode} from 'react';
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
-
-interface Props {
-    href: string;
-    children: ReactNode;
-}
 
 const Header: React.FC = () => {
     return (
-        <Navbar expand="lg" className="custom-navbar" sticky="top">
-            <Container>
-                <Navbar.Brand href="/" className="brand">Coordinator Helper</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto nav-links">
-                        <CustomLink href="/todolist">Список дел</CustomLink>
-                        <NavDropdown title="Скрипты" id="basic-nav-dropdown" className="nav-item-custom dropdown-custom">
-                            <NavDropdown.Item href="confirm" className="dropdown-item-custom">Подтверждение</NavDropdown.Item>
-                            <NavDropdown.Item href="confirm" className="dropdown-item-custom">Играли ранее?</NavDropdown.Item>
-                            <NavDropdown.Item href="confirm" className="dropdown-item-custom">Наличие экрана?</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="schedule" className="nav-item-custom">Расписание</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <nav className="navbar navbar-expand-lg custom-navbar sticky-top">
+            <div className="container">
+                <Link to="/" className="navbar-brand brand">Coordinator Helper</Link>
+
+                <button
+                    className="navbar-toggler custom-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 nav-links">
+                        <li className="nav-item nav-item-custom">
+                            <Link to="/todolist" className="nav-link">Список дел</Link>
+                        </li>
+
+                        <li className="nav-item dropdown nav-item-custom dropdown-custom">
+                            <span className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Скрипты
+                            </span>
+                            <ul className="dropdown-menu show-on-hover">
+                                <li>
+                                    <Link to="/confirm" className="dropdown-item dropdown-item-custom">Подтверждение</Link>
+                                </li>
+                                <li>
+                                    <Link to="/confirm" className="dropdown-item dropdown-item-custom">Играли ранее?</Link>
+                                </li>
+                                <li>
+                                    <Link to="/confirm" className="dropdown-item dropdown-item-custom">Наличие экрана?</Link>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li className="nav-item nav-item-custom">
+                            <Link to="/schedule" className="nav-link">Расписание</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
-
-function CustomLink({href, children}: Props) {
-    return (
-        <Nav.Link href={href} className="nav-item-custom">{children}</Nav.Link>
-    )
-}
 
 export default Header;
